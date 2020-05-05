1a87db2dcfa47c7ff1c47948dbbb24e7fdc45101
sravani <allenkisravani996@gmail.com>
2020-05-05 23:26:41 +0530

    entering first name

--- registration.sh
+++ registration.sh
@@ -1,10 +1,11 @@
 #! /bin/bash -x
 echo "firstname"
 read firstname
-if [[ $firstname =~ ^[A-Z]/[a-z]{2}$ ]]
+if [[ $firstname =~ ^[A-Z]([a-z][a-z])+$ ]]
 then
  echo "valid name"
 else 
  echo "invalid name"
 fi
+
 
